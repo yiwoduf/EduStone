4f7ee9781a33c827dafe55fa05162f26ba5604d9
Jack <jackkillschool@gmail.com>
2022-09-23 18:27:59 -0500

    Added stylesheets to pages

--- screens/User.js
+++ screens/User.js
@@ -1,10 +1,24 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
-const User = () => (
-  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <Text>User</Text>
-  </View>
-);
+const User = () => {
+  return(
+    <View style={styles.wrapper}>
+      <Text style={styles.text}>User</Text>
+    </View>
+  );
+}
 
 export default User;
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1d2951"
+  },
+  text: {
+    color: "#fff"
+  }
+});
