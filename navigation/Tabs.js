31bff40437a4b5cd99a49885d55b796f7e7fc8c8
leeable <37748514+yiwoduf@users.noreply.github.com>
2022-09-26 15:00:40 -0500

    UI Overlay for All except calendar page
    - UI overlay has been applied to all tabs except calendar
    
    - Tab order has been fixed
    
    - Tab names has been modified

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -6,13 +6,15 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
 import Calendar from "../screens/Calendar";
 import Todos from "../screens/Todos";
-import User from "../screens/User";
+import Stone from "../screens/Stone";
+import Profile from "../screens/Profile";
+import Award from "../screens/Award";
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => (
   <Tab.Navigator
-    initialRouteName="Todos"
+    initialRouteName="Stone"
     screenOptions={{
       headerShown: false,
       tabBarBackground: () => (
@@ -27,7 +29,9 @@ const Tabs = () => (
   >
     <Tab.Screen name="Calendar" component={Calendar} />
     <Tab.Screen name="Todos" component={Todos} />
-    <Tab.Screen name="User" component={User} />
+    <Tab.Screen name="Stone" component={Stone} />
+    <Tab.Screen name="Profile" component={Profile} />
+    <Tab.Screen name="Award" component={Award} />
   </Tab.Navigator>
 );
 
---
+++ screens/Stone.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import appStyles from "../components/appStyles";
+
+const Stone = () => {
+  return (
+    <View>
+      <Image
+        style={appStyles.ui}
+        source={require("../design/ui/UI_MainPage_iOS.png")}
+        resizeMode="contain"
+      />
+    </View>
+  );
+};
+
+export default Stone;
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1d2951",
+  },
+  text: {
+    color: "#fff",
+  },
+});
