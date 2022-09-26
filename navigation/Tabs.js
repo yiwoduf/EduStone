0422842e5d549d83ff1ec1b91b94793ec57992d6
pjw0194 <pjw019472@gmail.com>
2022-09-26 14:54:33 -0500

    demo dark mode transition

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,34 +1,47 @@
 import { View } from "react-native"; // For handling LinearGradient @lee
 import LinearGradient from "react-native-linear-gradient";
 import appStyles from "../components/appStyles";
-
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
 import Calendar from "../screens/Calendar";
 import Todos from "../screens/Todos";
 import User from "../screens/User";
+import { useColorScheme } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => (
-  <Tab.Navigator
-    initialRouteName="Todos"
-    screenOptions={{
-      headerShown: false,
-      tabBarBackground: () => (
-        <View style={{ flex: 1 }}>
-          <LinearGradient
-            colors={["#242424", "#3a2a31"]}
-            style={appStyles.linearGradient}
-          ></LinearGradient>
-        </View>
-      ),
-    }}
-  >
-    <Tab.Screen name="Calendar" component={Calendar} />
-    <Tab.Screen name="Todos" component={Todos} />
-    <Tab.Screen name="User" component={User} />
-  </Tab.Navigator>
-);
+const Tabs = () => {
+  const isDark = useColorScheme() === "dark";
+  return (
+    <Tab.Navigator
+      initialRouteName="Todos"
+      screenOptions={{
+        tabBarLabelStyle: isDark ? { color: "white" } : { color: "black" },
+        headerShown: false,
+        tabBarBackground: () => {
+          return isDark ? (
+            <View style={{ flex: 1 }}>
+              <LinearGradient
+                colors={["#242424", "#3a2a31"]}
+                style={appStyles.linearGradient}
+              ></LinearGradient>
+            </View>
+          ) : (
+            <View style={{ flex: 1 }}>
+              <LinearGradient
+                colors={["#f6fff8", "#ffe5d9"]}
+                style={appStyles.linearGradient}
+              ></LinearGradient>
+            </View>
+          );
+        },
+      }}
+    >
+      <Tab.Screen name="Calendar" component={Calendar} />
+      <Tab.Screen name="Todos" component={Todos} />
+      <Tab.Screen name="User" component={User} />
+    </Tab.Navigator>
+  );
+};
 
 export default Tabs;
