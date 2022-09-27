93735f95221440dbc4c388731fa6cd6e103e2bfb
pjw0194 <pjw019472@gmail.com>
2022-09-27 17:20:35 -0500

    toDoList tab basic design

--- pages/Todos.js
+++ pages/Todos.js
@@ -1,10 +1,16 @@
 import React from "react";
 import styleApp from "../style/styleApp";
-import { MainContainer, Header, HeaderTitle } from "../style/styleTodos";
+import {
+  MainContainer,
+  Header,
+  HeaderTitle,
+  BackgroundContainer,
+} from "../style/styleTodos";
 
 const Todos = () => {
   return (
     <MainContainer>
+      <BackgroundContainer source={require("../design/bg/ToDoList_BG.png")} />
       <Header>
         <HeaderTitle>To Do List</HeaderTitle>
       </Header>
--- style/styleTodos.js
+++ style/styleTodos.js
@@ -10,19 +10,30 @@ export const MainContainer = styled.View`
   display: flex;
   flex-direction: column;
   box-sizing: border-box;
-  padding-top: 15px;
+  height: ${height}px;
+  width: ${width}px;
 `;
 
-export const BackgroundContainer = styled.Image``;
+export const BackgroundContainer = styled.Image`
+  position: absolute;
+  z-index: -1;
+  max-height: ${height}px;
+  max-width: ${width}px;
+`;
 
 export const Header = styled.View`
   display: flex;
   justify-content: center;
-  height: ${height * 0.1};
-  width: ${width};
+  height: ${height * 0.2}px;
+  width: ${width}px;
   padding-left: 10px;
 `;
 
-export const HeaderTitle = styled.Text``;
+export const HeaderTitle = styled.Text`
+  letter-spacing: 2px;
+  color: white;
+  padding: 10px;
+  font-size: 30px;
+`;
 
 export const TodoListContainer = styled.View``;
