55f79d34910b10b0c0195cdd5b586683bc88e704
pjw0194 <pjw019472@gmail.com>
2022-09-27 22:53:33 -0500

    updated todo tab

--- style/styleTodos.js
+++ style/styleTodos.js
@@ -6,9 +6,10 @@ import styled from "styled-components/native";
 
 const { width, height } = Dimensions.get("window"); // Retrieve Information of Device Window Size
 
-export const MainContainer = styled.View`
+export const MainContainer = styled.SafeAreaView`
   display: flex;
   flex-direction: column;
+  align-items: center;
   box-sizing: border-box;
   height: ${height}px;
   width: ${width}px;
@@ -24,7 +25,7 @@ export const BackgroundContainer = styled.Image`
 export const Header = styled.View`
   display: flex;
   justify-content: center;
-  height: ${height * 0.2}px;
+  height: ${height * 0.1}px;
   width: ${width}px;
   padding-left: 10px;
 `;
@@ -32,8 +33,40 @@ export const Header = styled.View`
 export const HeaderTitle = styled.Text`
   letter-spacing: 2px;
   color: white;
-  padding: 10px;
+  padding-left: 10px;
   font-size: 30px;
 `;
 
-export const TodoListContainer = styled.View``;
+export const TodoListContainer = styled.FlatList`
+  height: 100%;
+  width: ${width * 0.9}px;
+`;
+
+export const TodoBox = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: ${width * 0.9}px;
+  height: 60px;
+  margin-bottom: 12px;
+  padding-left: 20px;
+  background-color: rgba(0, 0, 0, 0.3);
+`;
+
+export const Todo = styled.View``;
+
+export const TodoTitle = styled.Text`
+  font-size: 18px;
+  color: white;
+`;
+
+export const ProcessStatus = styled.View`
+  padding-right: 20px;
+`;
+
+export const DueDate = styled.Text`
+  color: white;
+  font-size: 16px;
+  letter-spacing: 1px;
+`;
