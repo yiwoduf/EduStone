85d43ceccc66b446e0e94265707883b3611cf0e9
pjw0194 <pjw019472@gmail.com>
2022-12-08 22:21:00 -0600

    update stone tab

--- style/styleStone.js
+++ style/styleStone.js
@@ -4,6 +4,7 @@
 import { Dimensions } from "react-native";
 import styled from "styled-components/native";
 import { basicFont, contentFontSize } from "./fonts";
+import LinearGradient from "react-native-linear-gradient";
 
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window"); // Retrieve Information of Device Window Size
 
@@ -14,6 +15,34 @@ export const MainContainer = styled.SafeAreaView`
   box-sizing: border-box;
   height: ${SCREEN_HEIGHT}px;
   width: ${SCREEN_WIDTH}px;
+`;
+
+export const Container = styled(LinearGradient).attrs({
+  colors: ["#9D4EDD", "#BC84E5", "#e89d65"],
+  start: { x: 1, y: 0 },
+  end: { x: 0, y: 1 },
+})`
+  flex: 1;
+  justify-content: space-between;
+`;
+
+export const PointsContainer = styled.View`
+  justify-content: center;
+  width: ${SCREEN_WIDTH * 0.8}px;
+  height: ${SCREEN_HEIGHT * 0.06}px;
+  background-color: rgba(0, 0, 0, 0.15);
+  border-radius: ${(SCREEN_HEIGHT * 0.06) / 3}px;
+  margin-bottom: 20px;
+`;
+
+export const PointsText = styled.Text`
+  font-family: ${basicFont};
+  color: white;
+  text-align: center;
+  font-size: ${SCREEN_WIDTH * 0.09}px;
+  text-shadow-color: #fff;
+  text-shadow-opacity: 50%;
+  text-shadow-radius: 6px;
 `;
 
 export const BackgroundContainer = styled.Image`
@@ -39,9 +68,13 @@ export const HeaderTitle = styled.Text`
 `;
 
 export const StoneExpContainer = styled.View`
-  display: flex;
-  flex-direction: column;
   align-items: center;
+  justify-content: center;
+  width: ${SCREEN_WIDTH * 0.8}px;
+  height: ${SCREEN_WIDTH * 0.8}px;
+  background-color: rgba(0, 0, 0, 0.15);
+  border-radius: ${(SCREEN_HEIGHT * 0.06) / 3}px;
+  margin-bottom: 20px;
 `;
 
 export const StoneExpTextCurrent = styled.Text`
@@ -58,19 +91,59 @@ export const StoneExpTextMax = styled.Text`
   font-family: ${basicFont};
   color: white;
   font-size: ${SCREEN_WIDTH * 0.1}px;
+  margin-top: 20px;
 `;
 
-export const StoneImageContainer = styled.View`
-  shadow-color: #ff5bda;
-  shadow-opacity: 0.4;
-  shadow-radius: 20px;
+export const MoreInfoContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: ${SCREEN_WIDTH * 0.8}px;
+  height: ${SCREEN_HEIGHT * 0.09}px;
+  background-color: rgba(0, 0, 0, 0.15);
+  border-radius: ${(SCREEN_HEIGHT * 0.06) / 3}px;
+  padding: 0px 25px;
+  margin: 15px 0px;
 `;
 
-export const StoneImage = styled.Image`
-  margin-top: ${SCREEN_HEIGHT * 0.1}px;
-  max-height: ${SCREEN_HEIGHT * 0.27}px;
-  max-width: ${SCREEN_WIDTH * 0.35}px;
+export const NoticeContainer = styled.View``;
+
+export const NoticeText = styled.Text`
+  font-family: ${basicFont};
+  color: white;
+  font-size: ${SCREEN_WIDTH * 0.055}px;
 `;
+
+export const DescriptText = styled.Text`
+  font-family: ${basicFont};
+  color: white;
+  font-size: ${SCREEN_WIDTH * 0.032}px;
+  opacity: 0.8;
+`;
+
+export const AssignmentNumText = styled.Text`
+  font-family: ${basicFont};
+  color: white;
+  font-size: ${SCREEN_WIDTH * 0.15}px;
+`;
+
+export const AssignmentProgressText = styled.Text`
+  font-family: ${basicFont};
+  color: white;
+  font-size: ${SCREEN_WIDTH * 0.082}px;
+`;
+
+// export const StoneImageContainer = styled.View`
+//   shadow-color: #ff5bda;
+//   shadow-opacity: 0.4;
+//   shadow-radius: 20px;
+// `;
+
+// export const StoneImage = styled.Image`
+//   margin-top: ${SCREEN_HEIGHT * 0.1}px;
+//   max-height: ${SCREEN_HEIGHT * 0.27}px;
+//   max-width: ${SCREEN_WIDTH * 0.35}px;
+// `;
 
 // export const ExpProgressBarMax = styled.View`
 //   width: ${width * 0.13}px;
