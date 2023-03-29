ffa59b48b88eb57f856ba23ad50a1cd09512ac20
pjw0194 <pjw019472@gmail.com>
2023-03-29 15:45:31 -0500

    Award page fully updated

--- style/styleAward.js
+++ style/styleAward.js
@@ -3,9 +3,19 @@
 
 import { Dimensions } from "react-native";
 import styled from "styled-components/native";
-import { basicFont, contentFontSize } from "./fonts";
+import { basicFont, boldFont } from "./fonts";
+import LinearGradient from "react-native-linear-gradient";
 
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window"); // Retrieve Information of Device Window Size
+
+export const Container = styled(LinearGradient).attrs({
+  colors: ["#AF52FA", "#B765E9", "#FFAA20"],
+  start: { x: -1, y: 0.5 },
+  end: { x: 1, y: 1 },
+})`
+  flex: 1;
+  justify-content: space-between;
+`;
 
 export const MainContainer = styled.SafeAreaView`
   display: flex;
@@ -16,95 +26,82 @@ export const MainContainer = styled.SafeAreaView`
   width: ${SCREEN_WIDTH}px;
 `;
 
-export const BackgroundContainer = styled.Image`
-  position: absolute;
-  z-index: -1;
-  max-height: ${SCREEN_HEIGHT}px;
-  max-width: ${SCREEN_WIDTH}px;
-`;
-
 export const Header = styled.View`
-  display: flex;
-  justify-content: center;
-  height: ${SCREEN_HEIGHT * 0.08}px;
-  width: ${SCREEN_WIDTH}px;
-  padding-left: ${SCREEN_WIDTH * 0.03}px;
+  width: 100%;
+  padding: ${SCREEN_WIDTH * 0.07}px;
 `;
 
 export const HeaderTitle = styled.Text`
-  font-family: ${basicFont};
-  color: white;
-  padding-left: ${SCREEN_WIDTH * 0.03}px;
-  font-size: ${SCREEN_WIDTH * 0.07}px;
+  font-family: ${boldFont};
+  font-size: 27px;
+  color: #ffffff;
 `;
 
-export const BadgeContainer = styled.View`
+export const DefaultContainer = styled.View`
+  position: relative;
   display: flex;
-  flex-wrap: wrap;
   flex-direction: row;
-  align-items: center;
   justify-content: space-between;
+  align-items: center;
+  width: ${SCREEN_WIDTH * 0.89}px;
+  background-color: rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  padding-left: ${SCREEN_WIDTH * 0.035}px;
+  padding-right: ${SCREEN_WIDTH * 0.035}px;
+  margin-bottom: ${SCREEN_HEIGHT * 0.01}px;
+`;
+
+export const BadgeContainer = styled(DefaultContainer)`
+  height: ${SCREEN_HEIGHT * 0.22}px;
+`;
+
+export const BadgeBox = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: ${SCREEN_WIDTH * 0.17}px;
+  height: ${SCREEN_WIDTH * 0.165}px;
+  background-color: white;
+  border-radius: 10px;
+  margin-right: ${SCREEN_WIDTH * 0.01}px;
+  margin-bottom: ${SCREEN_HEIGHT * 0.01}px;
+  margin-top: ${SCREEN_HEIGHT * 0.01}px; ;
 `;
 
 export const BadgeImage = styled.Image`
-  margin: ${SCREEN_WIDTH * 0.03}px;
-  justify-content: center;
-  align-items: center;
-  max-height: ${SCREEN_WIDTH * 0.16}px;
-  max-width: ${SCREEN_WIDTH * 0.16}px;
+  width: 35px;
+  height: 40px;
 `;
 
-export const SectionTitle = styled.View`
+export const BadgeHistoryContainer = styled.View`
   display: flex;
-  height: ${SCREEN_HEIGHT * 0.06}px;
-  width: ${SCREEN_WIDTH}px;
   align-items: center;
-  margin-top: ${SCREEN_HEIGHT * 0.025}px;
+  width: 100%;
 `;
 
-export const SectionTitleText = styled.Text`
-  font-family: ${basicFont};
-  color: white;
-  padding-left: ${SCREEN_WIDTH * 0.03}px;
-  font-size: ${SCREEN_WIDTH * 0.06}px;
-`;
-
-export const AwardListContainer = styled.View`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding-bottom: ${SCREEN_HEIGHT * 0.1}px;
-`;
-
-export const AwardComponentContainer = styled.View`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-start;
-  height: ${SCREEN_HEIGHT * 0.08}px;
-  width: ${SCREEN_WIDTH * 0.93}px;
-  margin-bottom: ${SCREEN_WIDTH * 0.03}px;
-  background-color: rgba(0, 0, 0, 0.3);
-  border-radius: ${SCREEN_HEIGHT * 0.015}px;
-  padding-left: ${SCREEN_WIDTH * 0.05}px;
-`;
-
-export const AwardImage = styled.Image`
-  width: ${SCREEN_WIDTH * 0.1}px;
-  height: ${SCREEN_WIDTH * 0.1}px;
-`;
-
-export const AwardTitleContainer = styled.View`
-  position: relative;
+export const BadgeHistoryBox = styled(DefaultContainer)`
   display: flex;
   height: ${SCREEN_HEIGHT * 0.08}px;
-  width: ${SCREEN_WIDTH * 0.7}px;
-  align-items: center;
-  justify-content: center;
 `;
 
-export const AwardTitle = styled.Text`
+export const BadgeHistoryHeader = styled(Header)`
+  align-items: center;
+  padding: ${SCREEN_WIDTH * 0.03}px;
+  margin-bottom: ${SCREEN_HEIGHT * 0.01}px;
+`;
+
+export const BadgeHistoryHeaderTitle = styled(HeaderTitle)`
+  font-size: 22px;
+`;
+
+export const BadgeHistoryTextContainer = styled.View``;
+
+export const BadgeHistoryTitle = styled(HeaderTitle)`
+  font-size: 20px;
+`;
+
+export const BadgeHistoryDescription = styled.Text`
   font-family: ${basicFont};
   color: white;
-  font-size: ${SCREEN_WIDTH * 0.06}px;
+  font-size: 12px;
 `;
